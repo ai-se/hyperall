c65486dc15048d3409213c256f2888a4f798eef6
Vivek <vivekaxl@gmail.com>
2018-03-23 14:09:04 -0400

    run DE

--- DE/run.py
+++ DE/run.py
@@ -1,9 +1,10 @@
 
 def run_DE(train, test, perf_measure, learner):
     """
-    This function would take a train dataset and would find parameter using DE by performing CV with fold == 5.
+     This function would take a train dataset and would find parameter using DE by performing CV with fold == 5.
     :param train: file path of train
     :param test: file path of test
+    :param learner: 
     :param perf_measure: Accuracy, recall etc.
     :return: performance measure
     """
