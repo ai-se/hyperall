b0408e36d931e821fa7fb762e992970e5e457856
huytu7 <huyqtu7@gmail.com>
2018-04-14 21:04:30 -0400

    merging result files

--- DE/run_1.py
+++ DE/run_1.py
@@ -16,8 +16,6 @@ from params_grid import param_grid
 cwd = os.getcwd()
 data_path = os.path.join(cwd, "../Data/DefectPrediction/")
 print(data_path)
-data = ["ivy", "lucene", "poi", "synapse", "velocity",
-        "camel", "log4j", "ant", "xerces", "jedit"]
 
 
 def load_data(train, test, name):
@@ -33,9 +31,6 @@ def load_data(train, test, name):
     train_dep_column = train_dep_columns[-1]
     train_ds[train_dep_column] = train_ds[train_dep_column].apply(lambda x: 0 if x == 0 else 1)
 
-    #train_X = train_ds[train_indep_columns]
-    #train_Y = [0 if x == 0 else 1 for x in train_ds[train_dep_column]]
-
     # Setting up Testing Data
     test_ds = pd.concat([pd.read_csv(path) for path in test_paths], ignore_index=True)
     test_columns = [col for col in test_ds.columns if '$' in col]
@@ -45,18 +40,15 @@ def load_data(train, test, name):
     test_dep_column = test_dep_columns[-1]
     test_ds[test_dep_column] = test_ds[test_dep_column].apply(lambda x: 0 if x == 0 else 1)
 
-    #test_X = test_ds[test_indep_columns]
-    #test_Y = [0 if x == 0 else 1 for x in test_ds[test_dep_column]]
-    #assert (test_X.shape[0] == len(test_Y)), "Something is wrong"
-    #return [train_X.values, np.array(train_Y)], [test_X.values, np.array(test_Y)]
     return train_ds, test_ds
 
 
-def call_de(i, x, training_data, testing_data, fold_indexes, goal="Max", term="Early"):
+def call_de(i, x, training_data, testing_data, fold_indexes, goal="Max", term="Early", num_eval=25):
     train_data = training_data.ix[fold_indexes[0]].values
     tune_data = training_data.ix[fold_indexes[1]].values
     test_data = testing_data.values
-    de = DE(NP=5, GEN=5, Goal=goal, termination=term)
+    num_p = num_eval/5
+    de = DE(NP=num_p, GEN=5, Goal=goal, termination=term)
     v, pareto = de.solve(process, OrderedDict(param_grid[i]['learners_para_dic']),
                          param_grid[i]['learners_para_bounds'], param_grid[i]['learners_para_categories'],
                          param_grid[i]['model'], x, [train_data, tune_data])
@@ -72,7 +64,7 @@ def call_de(i, x, training_data, testing_data, fold_indexes, goal="Max", term="E
     return val_tune, params
 
 
-def run_DE(train, test, perf_measures, learners, name=''):
+def run_DE(train, test, perf_measures, learners, name='', repeats = 1, num_eval=25):
     """
      This function would take a train dataset and would find parameter using DE by performing CV with fold == 5.
     :param train: file path of train
@@ -83,17 +75,16 @@ def run_DE(train, test, perf_measures, learners, name=''):
     """
     seed(47)
     np.random.seed(47)
-    repeats = 20
     fold = 3
     train_df, test_df = load_data(train, test, name)
     train_df, test_df = train_df.iloc[:, 3:], test_df.iloc[:, 3:]
     final_dic={}
-    save_pickle_address = 'dump/' + "25_" + perf_measures[0] + "_" + train[-1] + '_early.pickle'
-    print(save_pickle_address)
 
     for x in perf_measures:
         temp = {}
         print(x)
+        save_pickle_address = 'dump_2/' + str(num_eval) + "_" + x + "_" + train[-1] + '_early.pickle'
+        print(save_pickle_address)
         for i in learners:
             l = []
             l1 = []
@@ -106,27 +97,37 @@ def run_DE(train, test, perf_measures, learners, name=''):
                 for train_index, tune_index in kf:
                     if x == "d2h":
                         val, params = call_de(i, x, train_df, test_df,
-                                              [train_index, tune_index], "Min", "Late")
+                                              [train_index, tune_index], "Min", "Late", num_eval=num_eval)
                     else:
                         val, params = call_de(i, x, train_df, test_df,
-                                              [train_index, tune_index], "Max", "Early")
+                                              [train_index, tune_index], "Max", "Early", num_eval=num_eval)
                     l.append(val)
                     l1.append(params)
             total_time = time.time() - start_time
             temp[param_grid[i]['model'].__name__] = [l, l1, total_time]
             print(temp)
         final_dic[x] = temp
-    with open(save_pickle_address, 'wb') as handle:
-        pickle.dump(final_dic, handle)
+        with open(save_pickle_address, 'wb') as handle:
+            pickle.dump(final_dic, handle)
 
 
 if __name__ == '__main__':
-    perf_measures = ["f1"]
+    perf_measures = ["f1", "precision"]
     learners = ["svm", "knn", "dt", "rf"]
-    for dataset in data:
-        datasets = [x[2] for x in os.walk("../Data/DefectPrediction/%s" % dataset)][0]
-        for i in range(len(datasets)-1):
-            run_DE([datasets[i]], [datasets[i+1]], perf_measures, learners, dataset)
+    data = {"ivy": ['ivy-1.1.csv', 'ivy-1.4.csv', 'ivy-2.0.csv'],
+            "lucene": ['lucene-2.0.csv', 'lucene-2.2.csv', 'lucene-2.4.csv'],
+            "poi": ['poi-1.5.csv', 'poi-2.0.csv', 'poi-2.5.csv', 'poi-3.0.csv'],
+            "synapse": ['synapse-1.0.csv', 'synapse-1.1.csv', 'synapse-1.2.csv'],
+            "velocity": ['velocity-1.4.csv', 'velocity-1.5.csv'],
+            "camel": ['camel-1.2.csv', 'camel-1.4.csv', 'camel-1.6.csv'],
+            "log4j": ['log4j-1.0.csv', 'log4j-1.1.csv', 'log4j-1.2.csv'],
+            "ant": ['ant-1.4.csv', 'ant-1.5.csv', 'ant-1.6.csv'],
+            "xerces": ['xerces-1.2.csv', 'xerces-1.3.csv'],
+            "jedit": ['jedit-4.1.csv', 'jedit-4.2.csv', 'jedit-4.3.csv']}
+
+    for dataset, datasets in data.items():
+        for i in range(len(datasets) - 1):
+            run_DE([datasets[i]], [datasets[i + 1]], perf_measures, learners, dataset, repeats=20, num_eval=100)
 
 
 
